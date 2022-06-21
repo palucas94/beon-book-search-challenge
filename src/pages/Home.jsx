c144d5818f862ae1463cdcaeb7e54c135ef809d0
Lucas Pontes Arruda <paluke94@gmail.com>
2022-06-21 09:50:24 -0300

    [feat] add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Table from '../components/Table/Table';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Table />
+    </div>
+  );
+}
+
+export default Home;
