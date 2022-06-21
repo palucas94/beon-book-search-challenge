7f5a8025ee69122b2122c7bbf741cb8e75d4b55d
Lucas Pontes Arruda <paluke94@gmail.com>
2022-06-21 09:51:07 -0300

    [feat] add fetchFilteredBooks

--- src/provider/BooksProvider.jsx
+++ src/provider/BooksProvider.jsx
@@ -1,20 +1,44 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useMemo, useState } from 'react';
 import BooksContext from '../context/BooksContext';
-import fetchAllBooks from '../services/booksAPI';
+import { fetchAllBooks, fetchFilteredBooks } from '../services/booksAPI';
+
+const initialInputValues = {
+  searchInput: '',
+  minYear: '',
+  maxYear: '',
+};
 
 export default function BooksProvider({ children }) {
-  const [allBooks, setAllBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [inputValues, setInputValues] = useState(initialInputValues);
+
+  const inputHandler = ({ target: { name, value } }) => {
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+  };
+
+  const searchBooks = async () => {
+    const filteredBooks = await fetchFilteredBooks(inputValues);
+    setBooks(filteredBooks);
+  };
 
   useEffect(() => {
     async function getBooks() {
-      const books = await fetchAllBooks();
-      setAllBooks(books);
+      const allBooks = await fetchAllBooks();
+      setBooks(allBooks);
     }
     getBooks();
   }, []);
 
-  const contextValue = useMemo(() => ({ allBooks }));
+  const contextValue = useMemo(() => ({
+    books,
+    inputValues,
+    inputHandler,
+    searchBooks,
+  }));
 
   return (
     <BooksContext.Provider value={contextValue}>{ children }</BooksContext.Provider>
--- src/services/booksAPI.js
+++ src/services/booksAPI.js
@@ -1,9 +1,19 @@
+/* eslint-disable no-param-reassign */
 const BOOKS_API_URL = 'http://localhost:4000/books';
 
-const fetchAllBooks = async () => {
+export const fetchAllBooks = async () => {
   const API_RESPONSE = await fetch(BOOKS_API_URL);
   const BOOKS = await API_RESPONSE.json();
   return BOOKS;
 };
 
-export default fetchAllBooks;
+export const fetchFilteredBooks = async ({ searchInput, minYear, maxYear }) => {
+  if (minYear === '') { minYear = -5000; }
+  if (maxYear === '') { maxYear = 5000; }
+
+  const API_RESPONSE = await fetch(
+    `${BOOKS_API_URL}/?q=${searchInput}&year_gte=${minYear}&year_lte=${maxYear}`,
+  );
+  const BOOKS = await API_RESPONSE.json();
+  return BOOKS;
+};
