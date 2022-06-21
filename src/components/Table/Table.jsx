315b7aec8f75b834ba55fa180059e3a7f593d8c2
Lucas Pontes Arruda <paluke94@gmail.com>
2022-06-21 13:14:57 -0300

    [feat] add book details in Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,7 +2,14 @@ import React, { useContext } from 'react';
 import BooksContext from '../../context/BooksContext';
 
 function Table() {
-  const { books } = useContext(BooksContext);
+  const { currentPageBooks } = useContext(BooksContext);
+
+  const showBookDetails = (title) => {
+    const id = `${title}-details`;
+    const div = document.getElementById(id);
+
+    div.classList.toggle('show-details');
+  };
 
   return (
     <table>
@@ -14,16 +21,30 @@ function Table() {
         <th>Ações</th>
       </tr>
 
-      {books.map(({
-        title, author, language, year,
+      {currentPageBooks.map(({
+        title, author, language, year, pages, country, link, imageLink,
       }) => (
-        <tr>
-          <td>{title}</td>
-          <td>{author}</td>
-          <td>{language}</td>
-          <td>{year}</td>
-          <td>Detalhes</td>
-        </tr>
+        <>
+          <tr key={title + year}>
+            <td>{title}</td>
+            <td>{author}</td>
+            <td>{language}</td>
+            <td>{year}</td>
+            <td>
+              <button type="button" onClick={() => showBookDetails(title)}>Detalhes</button>
+            </td>
+          </tr>
+          <div id={`${title}-details`} className="book-details">
+            <p>{`Pages: ${pages}`}</p>
+            <p>{`Country: ${country}`}</p>
+            <p>
+              Link:
+              {' '}
+              <a href={link}>{link}</a>
+            </p>
+            <img src={imageLink} alt={title} />
+          </div>
+        </>
       ))}
     </table>
   );
