47f5f86927e17d2eedaf78f5dc121dd355690c32
Lucas Pontes Arruda <paluke94@gmail.com>
2022-06-21 09:48:00 -0300

    [feat] add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FilterBar from './FilterBar';
+import SearchBar from './SearchBar';
+
+function Header() {
+  return (
+    <>
+      <SearchBar />
+      <FilterBar />
+    </>
+  );
+}
+
+export default Header;
