e9ba9e8a7d46e23ccb90c83b4373f8f14ba02e84
Lucas Pontes Arruda <paluke94@gmail.com>
2022-06-21 07:19:14 -0300

    [feat] fetch books in provider

--- src/provider/BooksProvider.jsx
+++ src/provider/BooksProvider.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable react/prop-types */
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import BooksContext from '../context/BooksContext';
+import fetchAllBooks from '../services/booksAPI';
 
 export default function BooksProvider({ children }) {
-  const contextValue = useMemo(() => ({}), []);
+  const [allBooks, setAllBooks] = useState([]);
+
+  useEffect(() => {
+    async function getBooks() {
+      const books = await fetchAllBooks();
+      setAllBooks(books);
+    }
+    getBooks();
+  }, []);
+
+  const contextValue = useMemo(() => ({ allBooks }));
 
   return (
     <BooksContext.Provider value={contextValue}>{ children }</BooksContext.Provider>
