f564d0d09d1860bec4d6dfd9c62cf6a8baa83e2f
Lucas Pontes Arruda <paluke94@gmail.com>
2022-06-22 12:05:11 -0300

    [feat] add Table style

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import BooksContext from '../../context/BooksContext';
+import '../../styles/Table.css';
 
 function Table() {
   const { currentPageBooks } = useContext(BooksContext);
@@ -8,44 +9,58 @@ function Table() {
     const id = `${title}-details`;
     const div = document.getElementById(id);
 
-    div.classList.toggle('show-details');
+    div.classList.toggle('show-book-details');
   };
 
   return (
     <table>
-      <tr>
-        <th>Livro</th>
-        <th>Autor</th>
-        <th>Idioma</th>
-        <th>Ano</th>
-        <th>Ações</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>Livro</th>
+          <th>Autor</th>
+          <th>Idioma</th>
+          <th>Ano</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
 
-      {currentPageBooks.map(({
-        title, author, language, year, pages, country, link, imageLink,
-      }) => (
-        <>
-          <tr key={title + year}>
-            <td>{title}</td>
-            <td>{author}</td>
-            <td>{language}</td>
-            <td>{year}</td>
-            <td>
-              <button type="button" onClick={() => showBookDetails(title)}>Detalhes</button>
-            </td>
-          </tr>
-          <div id={`${title}-details`} className="book-details">
-            <p>{`Pages: ${pages}`}</p>
-            <p>{`Country: ${country}`}</p>
-            <p>
-              Link:
-              {' '}
-              <a href={link}>{link}</a>
-            </p>
-            <img src={imageLink} alt={title} />
-          </div>
-        </>
-      ))}
+      <tbody>
+        {currentPageBooks.map(({
+          title, author, language, year, pages, country, link,
+        }) => (
+          <>
+            <tr key={title + year}>
+              <td>{title}</td>
+              <td>{author}</td>
+              <td>{language}</td>
+              <td>{year}</td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => showBookDetails(title)}
+                  className="table__details-button"
+                >
+                  Detalhes
+                </button>
+              </td>
+            </tr>
+            <tr
+              id={`${title}-details`}
+              className="table__book-details"
+              key={link}
+            >
+              <td>{`Pages: ${pages}`}</td>
+              <td>{`Country: ${country}`}</td>
+              <td>
+                Link:
+                {' '}
+                <a href={link} target="_blank" rel="noreferrer">{link}</a>
+              </td>
+              {/* <img src={imageLink} alt={title} /> */}
+            </tr>
+          </>
+        ))}
+      </tbody>
     </table>
   );
 }
